c6496c3bc0aa37f852b26db51fbaa8497b096f61
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-17 20:29:11 -0700

    added the comments and links for the better understanding

--- src/MaxDisjointIntervals.java
+++ src/MaxDisjointIntervals.java
@@ -14,7 +14,7 @@ public class MaxDisjointIntervals {
         int currEnd = ans.get(0).get(1);
         for(int i=1; i<ans.size(); i++){
 
-            // if end of the first element si greater then this will be the part of the same interval,
+            // if end of the first element is greater, then this will be the part of the same interval,
             // so do not increase the count and update the end with minimum one
              if(currEnd >= ans.get(i).get(0)){
                  currEnd = Math.min(currEnd,ans.get(i).get(1));
